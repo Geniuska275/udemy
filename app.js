0789c004e2aaa26675d99c663a73b540fb7bf83a
geniuska <aigojie2020@gmail.com>
2022-04-22 11:04:06 +0100

    production preparation

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const app=express();
-app.set('view engine','ejs');
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-const port=process.env.PORT || 2000;
+const port = process.env.PORT || 2000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("udemy");
-})   
-app.listen(port, ()=>{
+})
+app.listen(port, () => {
     console.log("connected to database")
     console.log(`server listening to port  ${port}`);
-    })
+})
