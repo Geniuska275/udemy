497d7a866f6fd8674e2b08ba5752944b6709209a
geniuska <aigojie2020@gmail.com>
2022-04-22 10:15:06 +0100

    new commit

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
 app.set('view engine','ejs');
+app.use(express.static("public"));
 
 const port=2000
 
-
+app.get("/",(req,res)=>{
+    res.render("udemy");
+})   
 app.listen(port, ()=>{
     console.log("connected to database")
     console.log(`server listening to port  ${port}`);
     })
-
- app.get("/",(res,req)=>{
-     res.render("udemy");
- })   
